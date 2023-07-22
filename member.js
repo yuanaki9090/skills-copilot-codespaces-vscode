a00e3dec3673e330ec6f86c763a4a49d521e09a2
yuanaki9090 <65518397+yuanaki9090@users.noreply.github.com>
2023-07-22 16:14:23 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 30,
+        address: '123 Main St',
+        city: 'Miami',
+        state: 'FL'
+    };
+    return member;
+}
